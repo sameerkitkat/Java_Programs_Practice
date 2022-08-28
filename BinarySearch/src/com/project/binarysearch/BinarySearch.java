e92f006fba6015cbb3b3ae555a700466f80d8e6e
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2022-08-28 22:32:47 +0530

    Changing value in driver function

--- BinarySearch/src/com/project/binarysearch/BinarySearch.java
+++ BinarySearch/src/com/project/binarysearch/BinarySearch.java
@@ -28,6 +28,6 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 		int[] arr = { 10, 20, 30, 40, 50, 60 };
-		System.out.println(binarySearch(arr, 000));
+		System.out.println(binarySearch(arr, 10));
 	}
 }
